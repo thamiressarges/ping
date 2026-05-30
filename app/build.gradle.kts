01be87f9582f7e61b7fc6b627c4accb17a62dbfa
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-05-30 18:54:59 -0400

    feat: adiciona dependências Retrofit, Gson Converter e Coil Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,10 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
